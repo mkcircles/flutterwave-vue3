5d9724e1ef95cb7b1b6f3396784bc7f4c7cf06e4
mkcircles <mkcircles@gmail.com>
2025-08-26 09:41:48 +0300

    Commented out this import as its causing a build error in vue3

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import Flutterwave from "./Flutterwave";
 import useFlutterwave from "./useFlutterwave";
-import PaymentData from "types/PaymentData";
+//import PaymentData from "types/PaymentData";
 
 export default Flutterwave
 
 export {
     useFlutterwave,
-    PaymentData
+    //PaymentData
 }
